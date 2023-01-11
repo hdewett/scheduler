30885d57912633f17c9d7e80a7f78144a411e558
Hilora Dewett <h.dewett@outlook.com>
2023-01-11 16:40:49 -0500

    add function to display correct number of remaining spots

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -1,10 +1,29 @@
 import React from "react";
+import classNames from "classnames";
+
+import "components/DayListItem.scss";
 
 export default function DayListItem(props) {
+
+  const dayClass = classNames('day-list__item', {
+    'day-list__item--selected': props.selected,
+    'day-list__item--full': props.spots === 0
+  });
+
+  function formatSpots(spots) {
+    if (spots === 0) {
+      return 'No spots remaining';
+    }
+    if (spots === 1) {
+      return '1 spot remaining'
+    }
+    return `${spots} spots`;
+  }
+
   return (
-    <li onClick={() => props.setDay(props.name)}>
-      <h2 className="text--regular">{props.name}</h2>
-      <h3 className="text--light">{props.spots} spots remaining</h3>
+    <li className={dayClass}  onClick={() => props.setDay(props.name)}>
+      <h2>{props.name}</h2>
+      <h3 className="day-list__item--full">{formatSpots()}</h3>
     </li>
   );
 }
