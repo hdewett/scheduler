022e8df0118ad29588a7fa0fd76a2e31b5ae9f68
Hilora Dewett <h.dewett@outlook.com>
2023-01-12 11:09:38 -0500

    create InterviewList component, css, and stories

--- src/components/InterviewerListItem.js
+++ src/components/InterviewerListItem.js
@@ -1,6 +1,6 @@
 import React from "react";
 import classNames from "classnames";
-import 'components/InterviewerListItem.scss';
+import "components/InterviewerListItem.scss";
 
 export default function InterviewerListItem(props) {
 
