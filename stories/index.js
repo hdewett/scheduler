ad6a6e6c2061a3a16b3e5d36e8cfd7cd91645e78
Hilora Dewett <h.dewett@outlook.com>
2023-01-12 15:43:59 -0500

    Add Empty component and stories

--- stories/index.js
+++ stories/index.js
@@ -12,6 +12,7 @@ import InterviewerListItem from "components/InterviewerListItem";
 import InterviewerList from "components/InterviewerList.js";
 import Appointment from "components/Appointment/index.js";
 import Header from "components/Appointment/Header";
+import Empty from "components/Appointment/Empty";
 
 //Button
 storiesOf("Button", module)
@@ -151,3 +152,4 @@ storiesOf("Appointment", module)
   .add("Appointment", () => <Appointment />)
   .add("Appointment with Time", () => <Appointment time="12pm" />)
   .add("Header", () => < Header time="12pm" />)
+  .add("Empty", () => < Empty onAdd={action("onAdd")} />);
