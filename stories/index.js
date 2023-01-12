e9b01e44b5b7225f94d8a7e0864808ba9532dd58
Hilora Dewett <h.dewett@outlook.com>
2023-01-12 15:56:36 -0500

    add Show component and stories

--- stories/index.js
+++ stories/index.js
@@ -13,6 +13,7 @@ import InterviewerList from "components/InterviewerList.js";
 import Appointment from "components/Appointment/index.js";
 import Header from "components/Appointment/Header";
 import Empty from "components/Appointment/Empty";
+import Show from "components/Appointment/Show";
 
 //Button
 storiesOf("Button", module)
@@ -152,4 +153,11 @@ storiesOf("Appointment", module)
   .add("Appointment", () => <Appointment />)
   .add("Appointment with Time", () => <Appointment time="12pm" />)
   .add("Header", () => < Header time="12pm" />)
-  .add("Empty", () => < Empty onAdd={action("onAdd")} />);
+  .add("Empty", () => < Empty onAdd={action("onAdd")} />)
+
+  .add("Show", () => < Show 
+  student={"Lydia Miller-Jones"} 
+  interviewer={interviewer.name} 
+  onEdit={action("onEdit")} 
+  onDelete={action("onDelete")} 
+  />)
