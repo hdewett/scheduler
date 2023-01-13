91e7b1b8f3d4d8101fd1bb7566c79856f6092f85
Hilora Dewett <h.dewett@outlook.com>
2023-01-12 20:11:33 -0500

    add api proxy and use axios to get api/days instead of hard coded data

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,27 +1,10 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import DayList from "./DayList";
 import Appointment from "./Appointment";
+import axios from "axios";
 
 import "components/Application.scss";
 
-
-const days = [
-  {
-    id: 1,
-    name: "Monday",
-    spots: 2,
-  },
-  {
-    id: 2,
-    name: "Tuesday",
-    spots: 5,
-  },
-  {
-    id: 3,
-    name: "Wednesday",
-    spots: 0,
-  },
-];
 
 const appointments = {
   "1": {
@@ -64,7 +47,13 @@ const appointments = {
 
 export default function Application(props) {
 
-  const [day, setDay] = useState("Monday");
+  const [days, setDays] = useState([]);
+  const [day, setDay] = useState([]);
+
+  useEffect(() => {
+    axios.get("/api/days").then(response => setDays(response.data));
+  }, []);
+
 
   return (
     <main className="layout">
