011928d94f2cd83c9a5fb82a50787b5eaec528f4
Hilora Dewett <h.dewett@outlook.com>
2023-01-13 00:26:35 -0500

    Create function to return all appointments for given day

---
+++ src/helpers/selectors.js
@@ -0,0 +1,17 @@
+//this func returns a list of the appointments for the given day
+export function getAppointmentsForDay(state, day) {
+  let selectedAppointments;
+  let appointmentData = [];
+  for (let selectedDay of state.days) {
+    if (selectedDay.name === day){
+      selectedAppointments = selectedDay.appointments;
+    }
+  }
+  if (!selectedAppointments){
+    return appointmentData;
+  }
+  for (let id of selectedAppointments){
+    appointmentData.push(state.appointments[id]);
+  }
+  return appointmentData;
+}
