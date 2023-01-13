2c6c8deb52cb106ee25ae1a91ba4a6ca0224b383
Hilora Dewett <h.dewett@outlook.com>
2023-01-13 14:03:56 -0500

    code cleanup

--- src/components/Application.js
+++ src/components/Application.js
@@ -6,49 +6,7 @@ import axios from "axios";
 import "components/Application.scss";
 import { getAppointmentsForDay } from "../helpers/selectors";
 
-// const appointments = {
-//   "1": {
-//     id: 1,
-//     time: "12pm",
-//   },
-//   "2": {
-//     id: 2,
-//     time: "1pm",
-//     interview: {
-//       student: "Lydia Miller-Jones",
-//       interviewer:{
-//         id: 3,
-//         name: "Sylvia Palmer",
-//         avatar: "https://i.imgur.com/LpaY82x.png",
-//       }
-//     }
-//   },
-//   "3": {
-//     id: 3,
-//     time: "2pm",
-//   },
-//   "4": {
-//     id: 4,
-//     time: "3pm",
-//     interview: {
-//       student: "Archie Andrews",
-//       interviewer:{
-//         id: 4,
-//         name: "Cohana Roy",
-//         avatar: "https://i.imgur.com/FK8V841.jpg",
-//       }
-//     }
-//   },
-//   "5": {
-//     id: 5,
-//     time: "4pm",
-//   }
-// };
-
 export default function Application(props) {
-
-  // const [days, setDays] = useState([]);
-  // const [day, setDay] = useState([]);
 
   const [state, setState] = useState({
     day: "Monday",
@@ -60,14 +18,6 @@ export default function Application(props) {
   const dailyAppointments = getAppointmentsForDay(state, state.day);
   console.log(state);
   
-  // const setDay = day => {
-  //   return setState({ ...state, day:day })};
-
-  // const setDays = (days) => {
-  //   setState(prev => setState({...prev, days}));
-  // }
-
-
   useEffect(() => {
     Promise.all([
       axios.get('/api/days'),
@@ -82,11 +32,6 @@ export default function Application(props) {
       }));
     });
   }, [])
-
-  // useEffect(() => {
-  //   axios.get("/api/days")
-  //     .then(response => setDays(response.data));
-  // }, []);
 
 
   return (
